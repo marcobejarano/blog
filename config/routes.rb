1ddcc3e5bb38a2dd23c0053bef77c5db20b73244
Marco <m_uno_p@hotmail.com>
2024-05-07 15:56:41 -0500

    Add images controller and its tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Rails.application.routes.draw do
     to: 'pages#show',
     slug: /[-a-z0-9+]*/,
     as: :page
+
   get 'search', to: 'search#index'
   get 'search/:year/:month',
     to: 'search#index',
     year: /\d{4}/,
     month: /\d{2}/
+
   get 'tags/:name',
     to: 'tags#show',
     name: /[-a-z0-9_+]*/,
     as: :tag
 
+  resources :images, only: :show
+  
   root "home#index"
 end
